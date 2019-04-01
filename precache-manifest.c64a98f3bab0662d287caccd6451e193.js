72d721c506351dd1ffacacffc32b5717ca8b9777
aereal <aereal@aereal.org>
2019-04-01 16:01:09 +0900

    Updates

---
+++ precache-manifest.c64a98f3bab0662d287caccd6451e193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "427857dbb6ac8e7bbbc4",
+    "url": "/nengo/static/js/runtime~main.1c4f8029.js"
+  },
+  {
+    "revision": "d7ddafa8172987d6d40e",
+    "url": "/nengo/static/js/main.a0d47ca2.chunk.js"
+  },
+  {
+    "revision": "4eac0f7b5e3d471aa5fe",
+    "url": "/nengo/static/js/2.a5125d37.chunk.js"
+  },
+  {
+    "revision": "d7ddafa8172987d6d40e",
+    "url": "/nengo/static/css/main.e0d9dc18.chunk.css"
+  },
+  {
+    "revision": "0ee32e092622533af983805f68d91f0d",
+    "url": "/nengo/index.html"
+  }
+];
